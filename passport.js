b2ad6564b90c5ffa1629816df3e91ef1d01e9988
Insoo Yun <49330930+YunInSoo@users.noreply.github.com>
2020-11-06 08:34:58 +0900

    Update passport.js

--- passport.js
+++ passport.js
@@ -26,6 +26,7 @@ const passportConfig = passport => {
         console.log("start");
         try {
           if (username) {
+            //이부분에서 나중에 username데이터가 DB에 들어가있는지 또는 검증이되는 username인지 체크하는 로직이 필요합니다.
             return done(null, {
               user_id: username,
               password,
